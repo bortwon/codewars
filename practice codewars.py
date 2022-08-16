9121e7c4369158964000e579f170b45a440da049
bortw <pikanicka@gmail.cim>
2022-08-16 18:44:29 +0300

    tasks 3, 4, 5, 6

--- practice codewars.py
+++ practice codewars.py
@@ -36,3 +36,64 @@ def rps(p1, p2):
     if beats[p2] == p1:
         return "Player 2 won!"
     return "Draw!"
+
+
+# Complete the function that accepts a string parameter, and reverses each word in the string. All spaces in the string
+# should be retained.
+#
+# Examples
+# "This is an example!" ==> "sihT si na !elpmaxe"
+# "double  spaces"      ==> "elbuod  secaps"
+
+def reverse_words(text):
+    return ' '.join([word[::-1] for word in text.split(" ")])
+
+
+# Create a function that returns the sum of the two lowest positive numbers given an array of minimum 4 positive integers.
+# No floats or non-positive integers will be passed.
+# For example, when an array is passed like [19, 5, 42, 2, 77], the output should be 7.
+# [10, 343445353, 3453445, 3453545353453] should return 3453455.
+
+def sum_two_smallest_numbers(num_list):
+    num_list.sort()
+    return num_list[0] + num_list[1]
+
+
+# Given an array of integers.
+# Return an array, where the first element is the count of positives numbers and the second element is sum of negative
+# numbers. 0 is neither positive nor negative.
+# If the input is an empty array or is null, return an empty array.
+#
+# Example
+# For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].
+
+def count_positives_sum_negatives(arr):
+    poz = 0
+    neg = 0
+    result = []
+    if arr == []:
+        return []
+    for i in arr:
+        if i == 0:
+            continue
+        elif i > 0:
+            poz += 1
+        else:
+            neg += i
+    result.append(poz)
+    result.append(neg)
+    return result
+
+
+# Write a function which calculates the average of the numbers in a given list.
+#
+# Note: Empty arrays should return 0.
+
+def find_average(numbers):
+    result = 0
+    if numbers == []:
+        return 0
+    for i in numbers:
+        result += i
+    return result/len(numbers)
+
