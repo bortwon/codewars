3bc076648d61e07813f6b7f463d49bbaf08d812d
bortw <pikanicka@gmail.cim>
2022-08-17 17:08:00 +0300

    12 task

--- practice codewars.py
+++ practice codewars.py
@@ -209,3 +209,35 @@ def unique_in_order(iterable):
             res.append(item)
     return res
 
+
+# Check to see if a string has the same amount of 'x's and 'o's. The method must return a boolean and be case insensitive.
+# The string can contain any char.
+#
+# Examples input/output:
+#
+# XO("ooxx") => true
+# XO("xooxx") => false
+# XO("ooxXm") => true
+# XO("zpzpzpp") => true // when no 'x' and 'o' is present should return true
+# XO("zzoo") => false
+
+def xo(s):
+    x = 0
+    o = 0
+    for i in s:
+        if i == 'X' or i == 'x':
+            x += 1
+        elif i == 'O' or i == 'o':
+            o += 1
+        else:
+            continue
+    if x == o:
+        return True
+    else:
+        return False
+
+
+def xo(s):
+    s = s.lower()
+    return s.count('x') == s.count('o')
+
