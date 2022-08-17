2151f82f6404053ea14ee760850fa08aa0ce3d74
bortw <pikanicka@gmail.cim>
2022-08-17 16:57:24 +0300

    11 task

--- practice codewars.py
+++ practice codewars.py
@@ -179,3 +179,33 @@ def sum_mix(arr):
 
 def sum_mix(arr):
     return sum(map(int, arr))
+
+
+
+# Implement the function unique_in_order which takes as argument a sequence and returns a list of items without any
+# elements with the same value next to each other and preserving the original order of elements.
+#
+# For example:
+#
+# unique_in_order('AAAABBBCCDAABBB') == ['A', 'B', 'C', 'D', 'A', 'B']
+# unique_in_order('ABBCcAD')         == ['A', 'B', 'C', 'c', 'A', 'D']
+# unique_in_order([1,2,2,3,3])       == [1,2,3]
+
+def unique_in_order(iter):
+    iter = list(iter)
+    result = []
+    for i in range(len(iter)):
+        if i == 0 or iter[i] != iter[i - 1]:
+            result.append(iter[i])
+        else:
+            continue
+    return result
+
+
+def unique_in_order(iterable):
+    res = []
+    for item in iterable:
+        if len(res) == 0 or item != res[-1]:
+            res.append(item)
+    return res
+
