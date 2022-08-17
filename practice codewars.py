727718638f182224e203b6f506b9b2ee96c4a640
bortw <pikanicka@gmail.cim>
2022-08-17 16:34:21 +0300

    10 task

--- practice codewars.py
+++ practice codewars.py
@@ -139,3 +139,43 @@ def solution(str):
 def string_to_number(s):
     return int(s)
 
+
+# Write function bmi that calculates body mass index (bmi = weight / height2).
+#
+# if bmi <= 18.5 return "Underweight"
+#
+# if bmi <= 25.0 return "Normal"
+#
+# if bmi <= 30.0 return "Overweight"
+#
+# if bmi > 30 return "Obese"
+
+def bmi(weight, height):
+    bmi = weight / height ** 2
+    if bmi <= 18.5:
+        return 'Underweight'
+    elif 18.5 <= bmi <= 25.0:
+        return 'Normal'
+    elif 25.0 <= bmi <= 30.0:
+        return 'Overweight'
+    else:
+        return 'Obese'
+
+
+# Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+#
+# Return your answer as a number.
+
+def sum_mix(arr):
+    total = 0
+    for i in arr:
+        if isinstance(i, int):
+            total += i
+        else:
+            i = int(i)
+            total += i
+    return total
+
+
+def sum_mix(arr):
+    return sum(map(int, arr))
