8f8a94535f14df6972567d90f003df8b5ecf8d7a
bortw <pikanicka@gmail.cim>
2022-08-18 18:27:09 +0300

    17 task. Friend or Foe?

--- practice codewars.py
+++ practice codewars.py
@@ -341,3 +341,31 @@ def make_negative( number ):
 
 def make_negative(number):
     return -abs(number)
+
+
+# Friend or Foe?
+#
+# Make a program that filters a list of strings and returns a list with only your friends name in it.
+#
+# If a name has exactly 4 letters in it, you can be sure that it has to be a friend of yours! Otherwise, you can be sure
+# he's not...
+#
+# Ex: Input = ["Ryan", "Kieran", "Jason", "Yous"], Output = ["Ryan", "Yous"]
+#
+# i.e.
+#
+# friend ["Ryan", "Kieran", "Mark"] `shouldBe` ["Ryan", "Mark"]
+# Note: keep the original order of the names in the output.
+
+def friend(x):
+    friends = []
+    for i in x:
+        if len(i) == 4:
+            friends.append(i)
+        else:
+            continue
+    return friends
+
+
+def friend(x):
+    return [f for f in x if len(f) == 4]
