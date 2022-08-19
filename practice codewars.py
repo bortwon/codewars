61aa4e90eb2ed74cabffc2f6db4de608b915854a
bortw <pikanicka@gmail.cim>
2022-08-19 05:00:38 +0300

    20 task. Persistent Bugger.

--- practice codewars.py
+++ practice codewars.py
@@ -417,3 +417,40 @@ def likes(names):
             return '{}, {} and {} like this'.format(names[0], names[1], names[2])
         else:
             return '{}, {} and {} others like this'.format(names[0], names[1], len(names) - 2)
+
+
+
+# Persistent Bugger.
+#
+# Write a function, persistence, that takes in a positive parameter num and returns its multiplicative persistence,
+# which is the number of times you must multiply the digits in num until you reach a single digit.
+#
+# For example (Input --> Output):
+#
+# 39 --> 3 (because 3*9 = 27, 2*7 = 14, 1*4 = 4 and 4 has only one digit)
+# 999 --> 4 (because 9*9*9 = 729, 7*2*9 = 126, 1*2*6 = 12, and finally 1*2 = 2)
+# 4 --> 0 (because 4 is already a one-digit number)
+
+def persistence(n):
+    n_str = str(n)
+    n_list = list(n_str)
+    if len(n_list) == 1:
+        return 0
+    count = 0
+    while len(n_list) != 1:
+        b = 1
+        for i in n_list:
+            b *= int(i)
+        count += 1
+        n_str = str(b)
+        n_list = list(n_str)
+    return count
+
+
+import operator
+def persistence(n):
+    i = 0
+    while n>=10:
+        n=reduce(operator.mul,[int(x) for x in str(n)],1)
+        i+=1
+    return i
