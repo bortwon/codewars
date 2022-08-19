fa334a2262a9b71b688d281e3955d1851a2d8eaf
bortw <pikanicka@gmail.cim>
2022-08-19 17:58:37 +0300

    22 task. 8 kyu. Are You Playing Banjo?

--- practice codewars.py
+++ practice codewars.py
@@ -470,3 +470,28 @@ def reverse_seq(n):
 def reverseseq(n):
     return list(range(n, 0, -1))
 
+
+
+# 8 kyu
+# Are You Playing Banjo?
+#
+# Create a function which answers the question "Are you playing banjo?".
+# If your name starts with the letter "R" or lower case "r", you are playing banjo!
+#
+# The function takes a name as its only argument, and returns one of the following strings:
+#
+# name + " plays banjo"
+# name + " does not play banjo"
+# Names given are always valid strings.
+
+def are_you_playing_banjo(name):
+    name = list(name)
+    if name[0] == 'R' or name[0] == 'r':
+        return '{} plays banjo'.format(''.join(name))
+    else:
+        return '{} does not play banjo'.format(''.join(name))
+
+
+def areYouPlayingBanjo(name):
+    return name + (' plays' if name[0].lower() == 'r' else ' does not play') + " banjo";
+
