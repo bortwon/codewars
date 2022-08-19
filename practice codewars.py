c76ed41def744ff8f9b4e027e17ceb20955a5008
bortw <pikanicka@gmail.cim>
2022-08-19 18:24:03 +0300

    23 task. 7 kyu. Complementary DNA

--- practice codewars.py
+++ practice codewars.py
@@ -493,5 +493,43 @@ def are_you_playing_banjo(name):
 
 
 def areYouPlayingBanjo(name):
-    return name + (' plays' if name[0].lower() == 'r' else ' does not play') + " banjo";
+    return name + (' plays' if name[0].lower() == 'r' else ' does not play') + " banjo"
 
+
+
+# 7 kyu
+# Complementary DNA
+#
+# Deoxyribonucleic acid (DNA) is a chemical found in the nucleus of cells and carries the "instructions" for the
+#     development and functioning of living organisms.
+#
+# If you want to know more: http://en.wikipedia.org/wiki/DNA
+#
+# In DNA strings, symbols "A" and "T" are complements of each other, as "C" and "G". Your function receives one side of
+# the DNA (string, except for Haskell); you need to return the other complementary side. DNA strand is never empty or
+# there is no DNA at all (again, except for Haskell).
+#
+# More similar exercise are found here: http://rosalind.info/problems/list-view/ (source)
+#
+# Example: (input --> output)
+#
+# "ATTGC" --> "TAACG"
+# "GTAT" --> "CATA"
+
+def DNA_strand(dna):
+    list = []
+    for i in dna:
+        if i == 'T':
+            list.append('A')
+        elif i == 'A':
+            list.append('T')
+        elif i == 'C':
+            list.append('G')
+        else:
+            list.append('C')
+    dna = ''.join(list)
+    return dna
+
+pairs = {'A':'T','T':'A','C':'G','G':'C'}
+def DNA_strand(dna):
+    return ''.join([pairs[x] for x in dna])
