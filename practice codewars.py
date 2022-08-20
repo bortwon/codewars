ebdcf650bfb74ced59a58b35cb61832595eb13bf
bortw <pikanicka@gmail.cim>
2022-08-20 22:34:22 +0300

    27 task. 6 kyu. Duplicate Encoder

--- practice codewars.py
+++ practice codewars.py
@@ -619,3 +619,38 @@ def minimum(arr):
 def maximum(arr):
     return max(arr)
 
+
+
+# 6 kyu
+# Duplicate Encoder
+#
+# The goal of this exercise is to convert a string to a new string where each character in the new string is "(" if that
+# character appears only once in the original string, or ")" if that character appears more than once in the original
+# string. Ignore capitalization when determining if a character is a duplicate.
+#
+# Examples
+# "din"      =>  "((("
+# "recede"   =>  "()()()"
+# "Success"  =>  ")())())"
+# "(( @"     =>  "))(("
+# Notes
+# Assertion messages may be unclear about what they display in some languages. If you read "...It Should encode XXX",
+# the "XXX" is the expected result, not the input!
+
+from collections import Counter
+def duplicate_encode(word):
+    str = []
+    word = word.lower()
+    word = list(word)
+    count = Counter(word)
+    for i in word:
+        if count[i] == 1:
+            str.append('(')
+        else:
+            str.append(')')
+    str = ''.join(str)
+    return str
+
+def duplicate_encode(word):
+    return "".join(["(" if word.lower().count(c) == 1 else ")" for c in word.lower()])
+
