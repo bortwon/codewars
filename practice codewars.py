a2c0c11bb2628d4862ef0b14f77af6312e05c37a
bortw <pikanicka@gmail.cim>
2022-08-20 23:59:52 +0300

    38 task. 8 kyu. Double Char

--- practice codewars.py
+++ practice codewars.py
@@ -893,3 +893,27 @@ def rental_car_cost(d):
         return d * 40 - 20
     else:
         return d * 40 - 50
+
+
+
+# 8 kyu
+# Double Char
+#
+# Given a string, you have to return a string in which each character (case-sensitive) is repeated once.
+#
+# Examples (Input -> Output):
+# * "String"      -> "SSttrriinngg"
+# * "Hello World" -> "HHeelllloo  WWoorrlldd"
+# * "1234!_ "     -> "11223344!!__  "
+
+# Good Luck!
+
+def double_char(s):
+    result = []
+    for i in s:
+        result.append(i * 2)
+    return ''.join(result)
+
+def double_char(s):
+    return ''.join(c * 2 for c in s)
+
