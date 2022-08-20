2379f1734da921a3768959590ef13637d2c02ca3
bortw <pikanicka@gmail.cim>
2022-08-20 22:30:39 +0300

    26 task. 8 kyu. Find Maximum and Minimum Values of a List

--- practice codewars.py
+++ practice codewars.py
@@ -597,3 +597,25 @@ def find_even_index(arr):
         if sum(arr[:i]) == sum(arr[i+1:]):
             return i
     return -1
+
+
+
+# 8 kyu
+# Find Maximum and Minimum Values of a List
+#
+# Your task is to make two functions ( max and min, or maximum and minimum, etc., depending on the language ) that receive a list of integers as input, and return the largest and lowest number in that list, respectively.
+#
+# Examples (Input -> Output)
+# * [4,6,2,1,9,63,-134,566]         -> max = 566, min = -134
+# * [-52, 56, 30, 29, -54, 0, -110] -> min = -110, max = 56
+# * [42, 54, 65, 87, 0]             -> min = 0, max = 87
+# * [5]                             -> min = 5, max = 5
+# Notes
+# You may consider that there will not be any empty arrays/vectors.
+
+def minimum(arr):
+    return min(arr)
+
+def maximum(arr):
+    return max(arr)
+
