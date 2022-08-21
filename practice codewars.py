a32f8644490207528a9b38eb38f4d4ff89abca91
bortw <pikanicka@gmail.cim>
2022-08-22 01:50:46 +0300

    44 task. 8 kyu. Convert a Number to a String!

--- practice codewars.py
+++ practice codewars.py
@@ -1107,3 +1107,20 @@ def array_diff(a, b):
 
 def array_diff(a, b):
     return [x for x in a if x not in b]
+
+
+
+# 8 kyu
+# Convert a Number to a String!
+#
+# We need a function that can transform a number (integer) into a string.
+#
+# What ways of achieving this do you know?
+#
+# Examples (input --> output):
+# 123  --> "123"
+# 999  --> "999"
+# -100 --> "-100"
+
+def number_to_string(num):
+    return str(num)
