e251848e7ae2da00836fd24ec3462935da8b1d80
bortw <pikanicka@gmail.cim>
2022-08-22 05:16:03 +0300

    47 task. 6 kyu. Sort the odd

--- practice codewars.py
+++ practice codewars.py
@@ -1266,3 +1266,36 @@ def find_next_square(sq):
     result = ((math.sqrt(sq) + 1) ** 2)
     return result if result % 1 == 0 else -1
 
+
+
+# 6 kyu
+# Sort the odd
+#
+# Task
+# You will be given an array of numbers. You have to sort the odd numbers in ascending order while leaving the even
+# numbers at their original positions.
+#
+# Examples
+# [7, 1]  =>  [1, 7]
+# [5, 8, 6, 3, 4]  =>  [3, 8, 6, 5, 4]
+# [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]  =>  [1, 8, 3, 6, 5, 4, 7, 2, 9, 0]
+
+def sort_array(arr):
+    dict = {}
+    result = []
+    for i, e in enumerate(arr):
+        if e % 2 == 0:
+            dict[i] = e
+        else:
+            result.append(e)
+    result = sorted(result)
+    for i in dict:
+        result.insert(i, dict[i])
+    return result
+
+# ???
+def sort_array(arr):
+  odds = sorted((x for x in arr if x%2 != 0), reverse=True)
+  return [x if x%2==0 else odds.pop() for x in arr]
+
+
