b3f3498f2480e1b3a0d14cc3a755b5f580cd2381
bortw <pikanicka@gmail.cim>
2022-08-23 02:53:57 +0300

    49 task. 6 kyu. Count characters in your string

--- practice codewars.py
+++ practice codewars.py
@@ -1299,3 +1299,22 @@ def sort_array(arr):
   return [x if x%2==0 else odds.pop() for x in arr]
 
 
+
+# 6 kyu
+# Count characters in your string
+#
+# The main idea is to count all the occurring characters in a string. If you have a string like aba, then the
+# result should be {'a': 2, 'b': 1}.
+#
+# What if the string is empty? Then the result should be empty object literal, {}.
+
+def count(string):
+    dict = {}
+    for i in string:
+        dict[i] = string.count(i)
+    return dict
+
+from collections import Counter
+
+def count(string):
+    return Counter(string)
