265a1001428007d795ecd24b14b34bb543386042
bortw <pikanicka@gmail.cim>
2022-08-28 04:41:12 +0300

    54 task. 7 kyu. Two to One

--- practice codewars.py
+++ practice codewars.py
@@ -1465,3 +1465,23 @@ def paperwork(n, m):
 def remove_every_other(my_list):
     return my_list[::2]
 
+def remove_every_other(my_list):
+    return [v for c,v in enumerate(my_list) if not c%2]
+
+
+# 7 kyu
+# Two to One
+
+# Take 2 strings s1 and s2 including only letters from a to z. Return a new sorted string, the longest possible,
+# containing distinct letters - each taken only once - coming from s1 or s2.
+#
+# Examples:
+# a = "xyaabbbccccdefww"
+# b = "xxxxyyyyabklmopq"
+# longest(a, b) -> "abcdefklmopqwxy"
+#
+# a = "abcdefghijklmnopqrstuvwxyz"
+# longest(a, a) -> "abcdefghijklmnopqrstuvwxyz"
+
+def longest(s1, s2):
+    return "".join(sorted([c for c in set(s1 + s2)]))
