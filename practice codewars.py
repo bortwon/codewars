29c38852115946c75ad8fb817391977680fc6441
bortw <pikanicka@gmail.cim>
2022-08-28 04:50:19 +0300

    56 task. 8 kyu. Switch it Up!

--- practice codewars.py
+++ practice codewars.py
@@ -1586,3 +1586,23 @@ def encrypt(text, n):
     for i in range(n):
         text = text[1::2] + text[::2]
     return text
+
+
+
+# 8 kyu
+# Switch it Up!
+
+# When provided with a number between 0-9, return it in words.
+#
+# Input :: 1
+#
+# Output :: "One".
+#
+# If your language supports it, try using a switch statement.
+
+nums = {0: 'Zero', 1: 'One', 2: 'Two', 3: 'Three', 4: 'Four', 5: 'Five', 6: 'Six', 7: 'Seven', 8: 'Eight', 9: 'Nine'}
+def switch_it_up(num):
+    return nums[num]
+
+def switch_it_up(n):
+    return ['Zero','One','Two','Three','Four','Five','Six','Seven','Eight','Nine'][n]
