a0c4cf73f64532aa7adc15e733e7ecc035b66d51
bortw <pikanicka@gmail.cim>
2022-08-31 00:33:30 +0300

    58 task. 6 kyu. Counting Duplicates

--- practice codewars.py
+++ practice codewars.py
@@ -1648,3 +1648,39 @@ def palindrome_chain_length(n):
 def palindrome_chain_length(n, count=0):
     if str(n) == str(n)[::-1]: return count
     else: return palindrome_chain_length(n + int(str(n)[::-1]), count+1)
+
+
+
+# 6 kyu
+# Counting Duplicates
+
+# Count the number of Duplicates
+# Write a function that will return the count of distinct case-insensitive alphabetic characters and numeric digits
+# that occur more than once in the input string. The input string can be assumed to contain only
+# alphabets (both uppercase and lowercase) and numeric digits.
+#
+# Example
+# "abcde" -> 0 # no characters repeats more than once
+# "aabbcde" -> 2 # 'a' and 'b'
+# "aabBcde" -> 2 # 'a' occurs twice and 'b' twice (`b` and `B`)
+# "indivisibility" -> 1 # 'i' occurs six times
+# "Indivisibilities" -> 2 # 'i' occurs seven times and 's' occurs twice
+# "aA11" -> 2 # 'a' and '1'
+# "ABBA" -> 2 # 'A' and 'B' each occur twice
+
+def duplicate_count(text):
+    dict = {}
+    count = 0
+    text_res = text.lower()
+    for i in text_res:
+        if i in dict:
+            dict[i] += 1
+        else:
+            dict[i] = 1
+    for key in dict:
+        if dict[key] > 1:
+            count += 1
+    return count
+
+def duplicate_count(s):
+  return len([c for c in set(s.lower()) if s.lower().count(c)>1])
