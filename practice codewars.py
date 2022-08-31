f7e707a7727aaa767fdbbf4af4846ca29c754b9f
bortw <pikanicka@gmail.cim>
2022-08-31 06:54:58 +0300

    59 task. 6 kyu. Delete occurrences of an element if it occurs more than n times

--- practice codewars.py
+++ practice codewars.py
@@ -1684,3 +1684,49 @@ def duplicate_count(text):
 
 def duplicate_count(s):
   return len([c for c in set(s.lower()) if s.lower().count(c)>1])
+
+
+
+# 6 kyu
+# Delete occurrences of an element if it occurs more than n times
+
+# Enough is enough!
+# Alice and Bob were on a holiday. Both of them took many pictures of the places they've been, and now they want
+# to show Charlie their entire collection. However, Charlie doesn't like these sessions, since the motif usually
+# repeats. He isn't fond of seeing the Eiffel tower 40 times.
+# He tells them that he will only sit for the session if they show the same motif at most N times. Luckily, Alice
+# and Bob are able to encode the motif as a number. Can you help them to remove numbers such that their list contains
+# each number only up to N times, without changing the order?
+#
+# Task
+# Given a list and a number, create a new list that contains each number of list at most N times, without reordering.
+# For example if the input number is 2, and the input list is [1,2,3,1,2,1,2,3], you take [1,2,3,1,2], drop
+# the next [1,2] since this would lead to 1 and 2 being in the result 3 times, and then take 3, which
+# leads to [1,2,3,1,2,3].
+# With list [20,37,20,21] and number 1, the result would be [20,37,21].
+
+
+# bad!!!!!!!!
+def delete_nth(order,max_e):
+    dict = {}
+    for i in order:
+        if i in dict:
+            dict[i] += 1
+        else:
+            dict[i] = 1
+    order.reverse()
+    for i in order:
+        if dict[i] > max_e:
+            while dict[i] != max_e:
+                order.remove(i)
+                dict[i] -= 1
+    order.reverse()
+    return order
+
+
+def delete_nth(order,max_e):
+    ans = []
+    for o in order:
+        if ans.count(o) < max_e: ans.append(o)
+    return ans
+
