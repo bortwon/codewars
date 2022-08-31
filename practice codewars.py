6fde0bfc575c22b64ba9619685a472e91ccb0fc6
bortw <pikanicka@gmail.cim>
2022-08-31 07:43:59 +0300

    62 task. 8 kyu. Abbreviate a Two Word Name

--- practice codewars.py
+++ practice codewars.py
@@ -1786,3 +1786,25 @@ def rot13(message):
 import codecs
 def rot13(message):
     return codecs.encode(message, 'rot_13')
+
+
+
+# 8 kyu
+# Abbreviate a Two Word Name
+
+# Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
+#
+# The output should be two capital letters with a dot separating them.
+#
+# It should look like this:
+#
+# Sam Harris => S.H
+#
+# patrick feeney => P.F
+
+def abbrev_name(name):
+    name = list(name.split())
+    return name[0][0].upper() + '.' + name[1][0].upper()
+
+def abbrevName(name):
+    return '.'.join(w[0] for w in name.split()).upper()
