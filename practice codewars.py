44dc6cc8ba8b3bfc8d55db8a24c518ce376f4daa
bortw <pikanicka@gmail.cim>
2022-08-31 08:21:57 +0300

    64 task. 7 kyu. Square Every Digit

--- practice codewars.py
+++ practice codewars.py
@@ -1847,3 +1847,24 @@ def two_sum(nums, target):
         if diff in d:
             return [d[diff], i]
         d[num] = i
+
+
+
+# 7 kyu
+# Square Every Digit
+
+# Welcome. In this kata, you are asked to square every digit of a number and concatenate them.
+#
+# For example, if we run 9119 through the function, 811181 will come out, because 92 is 81 and 12 is 1.
+#
+# Note: The function accepts an integer and returns an integer
+
+def square_digits(num):
+    res = ''
+    num = list(str(num))
+    for i in num:
+        res += str(int(i) ** 2)
+    return int(res)
+
+def square_digits(num):
+    return int(''.join(str(int(d)**2) for d in str(num)))
