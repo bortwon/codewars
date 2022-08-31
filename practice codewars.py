18ec3e158a7b2f9a288ef92fe4a54d370f4a2a6c
bortw <pikanicka@gmail.cim>
2022-08-31 07:12:49 +0300

    60 task. 8 kyu. I love you, a little , a lot, passionately ... not at all

--- practice codewars.py
+++ practice codewars.py
@@ -1730,3 +1730,25 @@ def delete_nth(order,max_e):
         if ans.count(o) < max_e: ans.append(o)
     return ans
 
+
+
+# 8 kyu
+# I love you, a little , a lot, passionately ... not at all
+
+# Who remembers back to their time in the schoolyard, when girls would take a flower and tear its petals, saying each
+# of the following phrases each time a petal was torn:
+#
+# I love you
+# a little
+# a lot
+# passionately
+# madly
+# not at all
+# When the last petal was torn there were cries of excitement, dreams, surging thoughts and emotions.
+#
+# Your goal in this kata is to determine which phrase the girls would say for a flower of a given number of petals,
+# where nb_petals > 0.
+
+def how_much_i_love_you(nb_petals):
+    list = ['not at all', 'I love you', 'a little', 'a lot', 'passionately', 'madly']
+    return list[nb_petals % 6]
