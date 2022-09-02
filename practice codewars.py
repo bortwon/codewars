a588f3d75e114f354ce15df12a5b67115a860eaf
bortw <pikanicka@gmail.cim>
2022-09-02 21:24:36 +0300

    65 task. 8 kyu. Sum without highest and lowest number

--- practice codewars.py
+++ practice codewars.py
@@ -1868,3 +1868,43 @@ def square_digits(num):
 
 def square_digits(num):
     return int(''.join(str(int(d)**2) for d in str(num)))
+
+
+
+# 8 kyu
+# Sum without highest and lowest number
+
+# Sum all the numbers of a given array ( cq. list ), except the highest and the lowest
+# element ( by value, not by index! ).
+#
+# The highest or lowest element respectively is a single element at each edge, even if there are more than one
+# with the same value.
+#
+# Mind the input validation.
+#
+# Example
+# { 6, 2, 1, 8, 10 } => 16
+# { 1, 1, 11, 2, 3 } => 6
+# Input validation
+# If an empty value ( null, None, Nothing etc. ) is given instead of an array, or the given array is an empty list or
+# a list with only 1 element, return 0.
+
+def sum_array(arr):
+    if arr is None or 0 <= len(arr) <= 2:
+        return 0
+    arr.sort()
+    arr.pop(0)
+    arr.pop(-1)
+    return sum(arr)
+
+
+def sum_array(arr):
+    return sum(sorted(arr)[1:-1]) if arr and len(arr) > 1 else 0
+
+
+def sum_array(arr):
+    if arr == None or len(arr) < 3:
+        return 0
+    return sum(arr) - max(arr) - min(arr)
+
+
