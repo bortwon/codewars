f945ac5a59abe6b1f0e7bb684dc8002fd49a1f9d
pika <pikanicka@gmail.com>
2022-09-03 02:17:56 +0300

    70 task. 8 kyu. Basic Mathematical Operations

--- practice codewars.py
+++ practice codewars.py
@@ -2019,3 +2019,33 @@ def number(lines):
 
 def number(lines):
     return [f"{counter}: {line}" for counter, line in enumerate(lines, start=1)]
+
+
+
+# 8 kyu
+# Basic Mathematical Operations
+
+# Your task is to create a function that does four basic mathematical operations.
+#
+# The function should take three arguments - operation(string/char), value1(number), value2(number).
+# The function should return result of numbers after applying the chosen operation.
+#
+# Examples(Operator, value1, value2) --> output
+# ('+', 4, 7) --> 11
+# ('-', 15, 18) --> -3
+# ('*', 5, 5) --> 25
+# ('/', 49, 7) --> 7
+
+def basic_op(operator, value1, value2):
+    if operator == '+':
+        return value1 + value2
+    elif operator == '-':
+        return value1 - value2
+    elif operator == '*':
+        return value1 * value2
+    else:
+        return value1 / value2
+
+
+def basic_op(operator, value1, value2):
+    return eval("{}{}{}".format(value1, operator, value2))
