eb7d083cd483cccea8f5dabf6584c1d6e20d681e
bortw <pikanicka@gmail.cim>
2022-09-02 23:56:43 +0300

    68 task. 6 kyu. Detect Pangram

--- practice codewars.py
+++ practice codewars.py
@@ -1967,3 +1967,31 @@ def high(x):
 
 def high(x):
     return max(x.split(), key=lambda k: sum(ord(c) - 96 for c in k))
+
+
+
+# 6 kyu
+# Detect Pangram
+#
+# A pangram is a sentence that contains every single letter of the alphabet at least once. For example, the
+# sentence "The quick brown fox jumps over the lazy dog" is a pangram, because it uses the letters A-Z at least
+# once (case is irrelevant).
+#
+# Given a string, detect whether or not it is a pangram. Return True if it is, False if not. Ignore numbers and
+# punctuation.
+
+import string
+
+
+def is_pangram(s):
+    s = s.lower()
+    letters = list(string.ascii_lowercase)
+    for i in letters:
+        if i not in s:
+            return False
+    return True
+
+import string
+
+def is_pangram(s):
+    return set(string.ascii_lowercase).issubset(s.lower())
