53f78589fba105862ebd020b0c41a6519df9e468
bortw <pikanicka@gmail.cim>
2022-09-02 21:25:37 +0300

    66 task. 8 kyu. All Star Code Challenge #18

--- practice codewars.py
+++ practice codewars.py
@@ -1908,3 +1908,30 @@ def sum_array(arr):
     return sum(arr) - max(arr) - min(arr)
 
 
+# 8 kyu
+# All Star Code Challenge #18
+
+# This Kata is intended as a small challenge for my students
+#
+# All Star Code Challenge #18
+#
+# Create a function that accepts 2 string arguments and returns an integer of the count of occurrences the 2nd argument
+# is found in the first one.
+#
+# If no occurrences can be found, a count of 0 should be returned.
+#
+# ("Hello", "o")  ==>  1
+# ("Hello", "l")  ==>  2
+# ("", "z")       ==>  0
+# Notes:
+#
+# The first argument can be an empty string
+# The second string argument will always be of length 1
+
+def str_count(strng, letter):
+    return strng.count(letter) if len(strng) > 0 else 0
+
+def strCount(string, letter):
+    return string.count(letter)
+
+
