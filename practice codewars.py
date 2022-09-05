003fc829cd6c3b6587ea067046cb876cfa15a7bb
bortw <pikanicka@gmail.cim>
2022-09-06 02:16:43 +0300

    77 task. 8 kyu. Total amount of points

--- practice codewars.py
+++ practice codewars.py
@@ -2291,3 +2291,43 @@ def domain_name(url):
 def get_age(age):
     return int(age[0])
 
+
+# 8 kyu
+# Total amount of points
+
+# Our football team finished the championship. The result of each match look like "x:y". Results of all matches are
+# recorded in the collection.
+#
+# For example: ["3:1", "2:2", "0:1", ...]
+#
+# Write a function that takes such collection and counts the points of our team in the championship. Rules for
+# counting points for each match:
+#
+# if x > y: 3 points
+# if x < y: 0 point
+# if x = y: 1 point
+# Notes:
+#
+# there are 10 matches in the championship
+# 0 <= x <= 4
+# 0 <= y <= 4
+
+# FUNDAMENTALSMATHEMATICSARRAYS
+
+def points(games):
+    count = 0
+    for i in games:
+        if int(i[0]) > int(i[2]):
+            count += 3
+        elif int(i[0]) < int(i[2]):
+            count += 0
+        else:
+            count += 1
+    return count
+
+def points(games):
+    result = 0
+    for item in games:
+        result += 3 if item[0] > item[2] else 0
+        result += 1 if item[0] == item[2] else 0
+    return result
