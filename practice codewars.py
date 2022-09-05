186a993a19eee3ca8cd8e6319b64353ef10ee650
bortw <pikanicka@gmail.cim>
2022-09-06 00:23:07 +0300

    70 task. 8 kyu. Thinkful - Logic Drills: Traffic light

--- practice codewars.py
+++ practice codewars.py
@@ -2019,3 +2019,24 @@ def number(lines):
 
 def number(lines):
     return [f"{counter}: {line}" for counter, line in enumerate(lines, start=1)]
+
+
+
+# 8 kyu
+# Thinkful - Logic Drills: Traffic light
+#
+# You're writing code to control your town's traffic lights. You need a function to handle each change from green,
+# to yellow, to red, and then to green again.
+#
+# Complete the function that takes a string as an argument representing the current state of the light and returns
+# a string representing the state the light should change to.
+#
+# For example, when the input is green, output should be yellow.
+
+def update_light(current):
+    dict  = {'red': 'green', 'yellow': 'red', 'green': 'yellow' }
+    return dict[current]
+
+def update_light(current):
+    color = ['green', 'yellow', 'red']
+    return color[(color.index(current)+1)%len(color)]
