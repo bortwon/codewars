d676673dd2a0d993ac6440a14dcb94243c1285f9
bortw <pikanicka@gmail.cim>
2022-09-07 02:40:02 +0300

    80 task. 8 kyu. Gravity Flip

--- practice codewars.py
+++ practice codewars.py
@@ -2379,3 +2379,46 @@ def positive_sum(arr):
 
 def positive_sum(arr):
     return sum(x for x in arr if x > 0)
+
+
+
+# 8 kyu
+# Gravity Flip
+# 3466491% of 2,0707,287 of 19,638LogicalX
+#  Python
+# 3.10
+# VIM
+# EMACS
+# Instructions
+# Output
+# If you've completed this kata already and want a bigger challenge, here's the 3D version
+#
+# Bob is bored during his physics lessons so he's built himself a toy box to help pass the time. The box is special
+# because it has the ability to change gravity.
+#
+# There are some columns of toy cubes in the box arranged in a line. The i-th column contains a_i cubes.
+# At first, the gravity in the box is pulling the cubes downwards. When Bob switches the gravity, it begins to
+# pull all the cubes to a certain side of the box, d, which can be either 'L' or 'R' (left or right). Below is
+# an example of what a box of cubes might look like before and after switching gravity.
+#
+# +---+                                       +---+
+# |   |                                       |   |
+# +---+                                       +---+
+# +---++---+     +---+              +---++---++---+
+# |   ||   |     |   |   -->        |   ||   ||   |
+# +---++---+     +---+              +---++---++---+
+# +---++---++---++---+         +---++---++---++---+
+# |   ||   ||   ||   |         |   ||   ||   ||   |
+# +---++---++---++---+         +---++---++---++---+
+# Given the initial configuration of the cubes in the box, find out how many cubes are in each of the n columns
+# after Bob switches the gravity.
+#
+# Examples (input -> output:
+# * 'R', [3, 2, 1, 2]      ->  [1, 2, 2, 3]
+# * 'L', [1, 4, 5, 3, 5 ]  ->  [5, 5, 4, 3, 1]
+
+def flip(d, a):
+    return sorted(a) if d == 'R' else sorted(a, reverse = True)
+
+def flip(d,a):
+    return sorted(a, reverse=d=='L')
