897d0ef8d6c1f23e9ffb60fceb4a9d6cdfea1cb0
bortw <pikanicka@gmail.cim>
2022-09-07 02:29:08 +0300

    79 task. 8 kyu. Sum of positive

--- practice codewars.py
+++ practice codewars.py
@@ -2358,3 +2358,24 @@ def to_jaden_case(string):
 
 import string
 toJadenCase = string.capwords
+
+
+
+# 8 kyu
+# Sum of positive
+
+# You get an array of numbers, return the sum of all of the positives ones.
+#
+# Example [1,-4,7,12] => 1 + 7 + 12 = 20
+#
+# Note: if there is nothing to sum, the sum is default to 0.
+
+def positive_sum(arr):
+    res = 0
+    for i in arr:
+        if i > 0:
+            res += i
+    return res
+
+def positive_sum(arr):
+    return sum(x for x in arr if x > 0)
