3061db7ce467ae1385c08e5b818591df4f7c620b
bortw <pikanicka@gmail.cim>
2022-09-07 04:05:46 +0300

    81 task. 6 kyu. Gravity Flip (3D version)

--- practice codewars.py
+++ practice codewars.py
@@ -2422,3 +2422,34 @@ def flip(d, a):
 
 def flip(d,a):
     return sorted(a, reverse=d=='L')
+
+
+
+# ???
+# 6 kyu
+# Gravity Flip (3D version)
+
+# This kata is a slightly harder version of Gravity Flip. It is recommended to do that first.
+#
+# Bob is bored in his physics lessons yet again, and this time, he's brought a more complex gravity-changing box
+# with him. It's 3D, with small cubes arranged in a matrix of n×m columns. It can change gravity to go in a certain
+# direction, which can be 'L', 'R', 'D', and 'U' (left, right, down, and up).
+#
+# Given the initial configuration of the cubes inside of the box as a 2D array, determine how the cubes are arranged
+# after Bob switches the gravity.
+#
+# See the sample tests for examples.
+#
+# PUZZLESARRAYS
+
+import numpy as np
+
+dir = {
+    'L': lambda a: np.sort(a)[:, ::-1],
+    'R': lambda a: np.sort(a),
+    'U': lambda a: np.sort(a, axis=0)[::-1, :],
+    'D': lambda a: np.sort(a, axis=0)
+}
+
+def flip(d, a):
+    return dir[d](np.array(a)).tolist()
