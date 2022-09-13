d16b02ca57d38679ddd2e534dbaa81dee12a660a
bortw <pikanicka@gmail.cim>
2022-09-14 00:19:24 +0300

    86 task. 6 kyu. Zero-plentiful Array

--- practice codewars.py
+++ practice codewars.py
@@ -2614,3 +2614,60 @@ regex = (
     '{6,}'         # length at least 6 chars
     '$'            # end line
 )
+
+
+
+# 6 kyu
+# Zero-plentiful Array
+
+# An array is called zero-plentiful if it contains multiple zeros, and every sequence of zeros is at least 4 items long.
+#
+# Your task is to return the number of zero sequences if the given array is zero-plentiful, oherwise 0.
+#
+# Examples
+# [0, 0, 0, 0, 0, 1]  -->  1
+# # 1 group of 5 zeros (>= 4), thus the result is 1
+#
+# [0, 0, 0, 0, 1, 0, 0, 0, 0]  -->  2
+# # 2 group of 4 zeros (>= 4), thus the result is 2
+#
+# [0, 0, 0, 0, 1, 0]  -->  0
+# # 1 group of 4 zeros and 1 group of 1 zero (< 4)
+# # _every_ sequence of zeros must be at least 4 long, thus the result is 0
+#
+# [0, 0, 0, 1, 0, 0]  -->  0
+# # 1 group of 3 zeros (< 4) and 1 group of 2 zeros (< 4)
+#
+# [1, 2, 3, 4, 5]  -->  0
+# # no zeros
+#
+# []  -->  0
+# # no zeros
+
+# FUNDAMENTALS
+
+def zero_plentiful(arr):
+    zero = 0
+    res = 0
+    if len(arr) == 1:
+        return 0
+    for i in arr:
+        if i == 0:
+            zero += 1
+        if i != 0:
+            if zero >= 4:
+                res += 1
+                zero = 0
+            elif zero != 0 and zero < 4:
+                return 0
+            elif zero == 0:
+                continue
+    if zero >= 4:
+        res += 1
+    return res
+
+
+from itertools import groupby
+def zero_plentiful(arr):
+    r = [len(list(g)) > 3 for k, g in groupby(arr) if k == 0]
+    return all(r) * len(r)
