5eec10c6e77ae9cdeb3447b7e69ede66458fdacd
bortw <pikanicka@gmail.cim>
2022-09-13 19:18:11 +0300

    85 task. 5 kyu. Regex Password Validation

--- practice codewars.py
+++ practice codewars.py
@@ -2574,3 +2574,43 @@ def func(arr, k):
                 continue
     else:
         return []
+
+
+
+# 5 kyu
+# Regex Password Validation
+
+# You need to write regex that will validate a password to make sure it meets the following criteria:
+#
+# At least six characters long
+# contains a lowercase letter
+# contains an uppercase letter
+# contains a digit
+# only contains alphanumeric characters (note that '_' is not alphanumeric)
+
+# REGULAR EXPRESSIONSFUNDAMENTALS
+
+regex="^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[a-zA-Z\d]{6,}$"
+
+
+from re import compile, VERBOSE
+regex = compile("""
+^              # begin word
+(?=.*?[a-z])   # at least one lowercase letter
+(?=.*?[A-Z])   # at least one uppercase letter
+(?=.*?[0-9])   # at least one number
+[A-Za-z\d]     # only alphanumeric
+{6,}           # at least 6 characters long
+$              # end word
+""", VERBOSE)
+
+
+regex = (
+    '^'            # start line
+    '(?=.*\d)'     # must contain one digit from 0-9
+    '(?=.*[a-z])'  # must contain one lowercase characters
+    '(?=.*[A-Z])'  # must contain one uppercase characters
+    '[a-zA-Z\d]'   # permitted characters (alphanumeric only)
+    '{6,}'         # length at least 6 chars
+    '$'            # end line
+)
