36ea80f38b01a8330ed92642ab712023922a7d19
pika <pikanicka@gmail.com>
2022-09-14 07:28:17 +0300

    88 task. 6 kyu. Write Number in Expanded Form

--- practice codewars.py
+++ practice codewars.py
@@ -2699,3 +2699,34 @@ def vaporcode(s):
 
 def vaporcode(s):
     return "  ".join(s.replace(" ", "").upper())
+
+
+
+# 6 kyu
+# Write Number in Expanded Form
+
+# Write Number in Expanded Form
+# You will be given a number and you will need to return it as a string in Expanded Form. For example:
+#
+# expanded_form(12) # Should return '10 + 2'
+# expanded_form(42) # Should return '40 + 2'
+# expanded_form(70304) # Should return '70000 + 300 + 4'
+# NOTE: All numbers will be whole numbers greater than 0.
+#
+# If you liked this kata, check out part 2!!
+#
+# STRINGS MATHEMATICS ALGORITHMS FUNDAMENTALS
+
+def expanded_form(num):
+    res = []
+    length = len(str(num)) - 1
+    for i in str(num):
+        if i != '0':
+            res.append(i + '0' * length)
+        length -= 1
+    return ' + '.join(res)
+
+
+def expanded_form(num):
+    num = list(str(num))
+    return ' + '.join(x + '0' * (len(num) - y - 1) for y,x in enumerate(num) if x != '0')
